4031b3a2dd356f2215b4757965f30b7f3be867c0
txwizard <txwizard@gmail.com>
2019-07-31 15:48:06 -0500

    Use a modified form of BTest.cpp, to which I added a few more print statements, as my demonstration program.

--- PSQLviaADO/PSQLviaADO/ADOHelpers.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include ".\PSQLviaADO.h"
-
-class ADOHelpers
-{
-public:
-	static _variant_t ParseAndConvert (IN _bstr_t pstrInputValue, IN FieldPtr pDBField);
-};
-
--- PSQLviaADO/PSQLviaADO/MessageResourceHelpers.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Windows.h>
-#include <Const_Typedefs_WW.H>
-
-LPTSTR __stdcall GetUserLocale ( );
-void   __stdcall DisplayMessage             ( HMODULE  phPEHandle , RES_STRING_ID puintResID   );
-LPTSTR __stdcall LoadMessage                ( HMODULE  phPEHandle , RES_STRING_ID puintResID   );
-int    __stdcall ReportSystemErrorOnConsole ( CHMODULE phPEHandle , CDWORD        pdwLastError );
